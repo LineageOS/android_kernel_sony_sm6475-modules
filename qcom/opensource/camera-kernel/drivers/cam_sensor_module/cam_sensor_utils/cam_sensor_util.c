3794573a96b156f88d234f6c770b4648218d6860
jmpfbmx <jmpf.bmx@gmail.com>
2026-02-04 18:53:35 +0100

    qcom: camera-kernel: sensor_util: Guard i2c list head access
    cam_sensor_util_*() may access i2c_list_head->prev without validating
    the list head, which can lead to a NULL dereference on error paths.
    
    Add a defensive check before using the list pointers.
    
    Change-Id: I95a7fac4853501c1a92d11debd42a02ceb3fcc15
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
+++ qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
@@ -175,6 +175,11 @@ int32_t cam_sensor_handle_random_write(
 {
 	struct i2c_settings_list  *i2c_list;
 	int32_t rc = 0, cnt, payload_count;
+	struct list_head  *i2c_list_head = &(i2c_reg_settings->list_head);
+
+	/* Null pointer judgment */
+	if(i2c_list_head->prev == NULL)
+			return -ENOMEM;
 
 	payload_count = cam_cmd_i2c_random_wr->header.count;
 	i2c_list = cam_sensor_get_i2c_ptr(i2c_reg_settings,
@@ -215,6 +220,11 @@ static int32_t cam_sensor_handle_continuous_write(
 {
 	struct i2c_settings_list *i2c_list;
 	int32_t rc = 0, cnt, payload_count;
+	struct list_head  *i2c_list_head = &(i2c_reg_settings->list_head);
+
+	/* Null pointer judgment */
+	if(i2c_list_head->prev == NULL)
+			return -ENOMEM;
 
 	payload_count = cam_cmd_i2c_continuous_wr->header.count;
 	i2c_list = cam_sensor_get_i2c_ptr(i2c_reg_settings,
